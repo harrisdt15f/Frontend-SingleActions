958e8c465d41c97324a8a8e537f384054b3c1fda
LingPh <953909858@qq.com>
2019-07-24 17:22:49 +0800

    用户  帐变记录&充值记录   API

--- User/Fund/UserFundAction.php
+++ User/Fund/UserFundAction.php
@@ -31,18 +31,14 @@ class UserFundAction
      */
     public function execute(FrontendApiMainController $contll): JsonResponse
     {
-        $user = $contll->partnerUser;
-        $eloqM = new FrontendUsersAccountsReport();
-        $contll->inputs['extra_where']['method'] = 'where';
-        $contll->inputs['extra_where']['key'] = 'user_id';
-        $contll->inputs['extra_where']['value'] = $user->id;
+        $contll->inputs['user_id'] = $contll->partnerUser->id;
         $fixedJoin = 1; //number of joining tables
         $withTable = 'gameMethods';
-        $searchAbleFields = ['issue', 'process_time','type_name','amount','balance','method_id'];
-        $withSearchAbleFields = ['method_id','lottery_name','method_name'];
+        $searchAbleFields = ['issue', 'process_time', 'type_name', 'amount', 'balance', 'method_id', 'user_id'];
+        $withSearchAbleFields = ['method_id', 'lottery_name', 'method_name'];
         $orderFields = 'id';
-        $orderFlow = 'asc';
-        $data = $contll->generateSearchQuery($eloqM, $searchAbleFields, $fixedJoin, $withTable, $withSearchAbleFields, $orderFields, $orderFlow);
+        $orderFlow = 'desc';
+        $data = $contll->generateSearchQuery($this->model, $searchAbleFields, $fixedJoin, $withTable, $withSearchAbleFields, $orderFields, $orderFlow);
         return $contll->msgOut(true, $data);
     }
 }
--- User/Fund/UserRechargeListAction.php
+++ User/Fund/UserRechargeListAction.php
@@ -6,7 +6,6 @@
 
 namespace App\Http\SingleActions\Frontend\User\Fund;
 
-
 use App\Http\Controllers\FrontendApi\FrontendApiMainController;
 use App\Models\User\UsersRechargeHistorie;
 use Illuminate\Http\JsonResponse;
@@ -30,13 +29,11 @@ class UserRechargeListAction
      */
     public function execute(FrontendApiMainController $contll): JsonResponse
     {
-        $user = $contll->partnerUser;
-        $eloqM = new UsersRechargeHistorie();
-        $contll->inputs['extra_where']['method'] = 'where';
-        $contll->inputs['extra_where']['key'] = 'user_id';
-        $contll->inputs['extra_where']['value'] = $user->id;
-        $searchAbleFields = [ 'company_order_num','created_at','amount'];
-        $data = $contll->generateSearchQuery($eloqM, $searchAbleFields);
+        $contll->inputs['user_id'] = $contll->partnerUser->id;
+        $searchAbleFields = ['user_id', 'company_order_num', 'created_at', 'amount'];
+        $orderFields = 'id';
+        $orderFlow = 'desc';
+        $data = $contll->generateSearchQuery($this->model, $searchAbleFields, 0, null, null, $orderFields, $orderFlow);
         return $contll->msgOut(true, $data);
     }
 }
