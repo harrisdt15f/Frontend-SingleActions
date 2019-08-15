b5e1daeb06189f97e81f07c60283acb292e94292
three <three@qq.com>
2019-08-15 03:51:13 -0400

    鏈接開戶-鏈接刪除

--- FrontendAuthRegisterAction.php
+++ FrontendAuthRegisterAction.php
@@ -10,6 +10,7 @@ use App\Models\User\FrontendLinksRegisteredUsers;
 use App\Models\User\UserPublicAvatar;
 use App\Models\User\FrontendUser;
 use App\Models\User\Fund\FrontendUsersAccount;
+use App\Models\User\FrontendUsersSpecificInfo;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +28,7 @@ class FrontendAuthRegisterAction
     {
         $this->model = $frontendUser;
     }
-    
+
     /**
      * 用户注册
      * 0.普通注册
@@ -38,96 +39,109 @@ class FrontendAuthRegisterAction
      * @param $inputDatas
      * @return JsonResponse
      */
-    public function execute(FrontendApiMainController $contll,$inputDatas): JsonResponse
+    public function execute(FrontendApiMainController $contll, $inputDatas): JsonResponse
     {
         //注册类型
         $registerType = $inputDatas['register_type'] ?? 0;
         $re_password = $inputDatas['re_password'] ?? '';
-        
-        if($re_password != '' && $re_password != $inputDatas['password']){
+
+        if ($re_password != '' && $re_password != $inputDatas['password']) {
             return $contll->msgOut(false, [], '100008');
         }
-        
-        $typeArr = [0,1,2,3];
-        if(!in_array($registerType,$typeArr)){
+
+        $typeArr = [0, 1, 2, 3];
+        if (!in_array($registerType, $typeArr)) {
             $registerType = 0;
         }
 
-        $inputDatas['register_type'] = $registerType;
         $inputDatas['vip_level'] = 0;
         $inputDatas['parent_id'] = 0;
-        
+
         //1.人工开户注册
-        if($registerType == 1){
+        if ($registerType == 1) {
             $inputDatas['prize_group'] = $inputDatas['prize_group'] ?? 0;
-            if($inputDatas['prize_group'] == 0){
+            if ($inputDatas['prize_group'] == 0) {
                 return $contll->msgOut(false, [], '100015');
             }
-            
+
             //当前用户需要登录
-            if(!Auth::check()){
+            if (!Auth::check()) {
                 return $contll->msgOut(false, [], '100019');
             }
-            
+
             $inputDatas['parent_id'] = Auth::id();
             $inputDatas['platform_id'] = $contll->currentPlatformEloq->platform_id;
             $inputDatas['platform_sign'] = $contll->currentPlatformEloq->platform_sign;
-            
+
             //最低开户奖金组
             $min_user_prize_group = SystemConfiguration::getConfigValue('min_user_prize_group');
             //最高开户奖金组
             $max_user_prize_group = SystemConfiguration::getConfigValue('max_user_prize_group');
 
-            $userInfo = $contll->currentAuth->user() ;
-            if($userInfo->prize_group < $max_user_prize_group ){
+            $userInfo = $contll->currentAuth->user();
+            if ($userInfo->prize_group < $max_user_prize_group) {
                 $max_user_prize_group = $userInfo->prize_group;
             }
 
-            if($inputDatas['prize_group'] < $min_user_prize_group || $inputDatas['prize_group'] > $max_user_prize_group){
+            if ($inputDatas['prize_group'] < $min_user_prize_group ||
+                $inputDatas['prize_group'] > $max_user_prize_group
+            ) {
                 return $contll->msgOut(false, [], '100016');
             }
         }
-        
+
         //2.链接开户注册和扫码开户
-        if($registerType == 2 || $registerType == 3){
+        if ($registerType == 2 || $registerType == 3) {
             $keyword = $inputDatas['keyword'] ?? '';
-            
-            if($keyword == ''){
-                return $contll->msgOut(false, [], '100017'); 
+
+            if ($keyword == '') {
+                return $contll->msgOut(false, [], '100017');
             }
-            
-            $link = FrontendUsersRegisterableLink::where('keyword',$keyword)->first();
-            
-            if(is_null($link)){
+
+            $link = FrontendUsersRegisterableLink::where('keyword', $keyword)->first();
+
+            if (is_null($link)) {
                 return $contll->msgOut(false, [], '100018');
             }
-            
+
             $inputDatas['prize_group'] = $link->prize_group;
             $inputDatas['parent_id'] = $link->user_id;
             $inputDatas['platform_id'] = $link->platform_id;
             $inputDatas['platform_sign'] = $link->platform_sign;
         }
-        
+
         //验证平台信息是否存在
-        $platform = SystemPlatform::where('platform_id',$inputDatas['platform_id'])->where('platform_sign',$inputDatas['platform_sign'])->first();
-        if(is_null($platform)){
+        $platform = SystemPlatform::where('platform_id', $inputDatas['platform_id'])
+            ->where('platform_sign', $inputDatas['platform_sign'])
+            ->first();
+        if (is_null($platform)) {
             return $contll->msgOut(false, [], '100020');
         }
-        
+
         $inputDatas['password'] = bcrypt($inputDatas['password']);
         $inputDatas['register_ip'] = request()->ip();
         $inputDatas['pic_path'] = UserPublicAvatar::getRandomAvatar();
         $inputDatas['sign'] = $inputDatas['platform_sign'];
-        
+
         //删除不必要的数据
         unset($inputDatas['keyword']);
         unset($inputDatas['platform_sign']);
-        
+
         //插入信息
         DB::beginTransaction();
         try {
+            //附属信息
+            $FrontendUsersSpecificInfo = new FrontendUsersSpecificInfo();
+            $SpecificInfo = [
+                'register_type' => $registerType,
+            ];
+            $FrontendUsersSpecificInfo = $FrontendUsersSpecificInfo->fill($SpecificInfo);
+            $FrontendUsersSpecificInfo->save();
+            $inputDatas['user_specific_id'] = $FrontendUsersSpecificInfo->id;
             $user = $this->model::create($inputDatas);
             $user->rid = $user->id;
+
+            //账户信息
             $userAccountEloq = new FrontendUsersAccount();
             $userAccountData = [
                 'user_id' => $user->id,
@@ -139,9 +153,10 @@ class FrontendAuthRegisterAction
             $userAccountEloq->save();
             $user->account_id = $userAccountEloq->id;
             $user->save();
-            
+
+
             //链接开户，扫码开户
-            if($registerType == 2 || $registerType == 3){
+            if ($registerType == 2 || $registerType == 3) {
                 $registeredUser = new FrontendLinksRegisteredUsers;
                 $registeredUser->register_link_id = $link->id;
                 $registeredUser->user_id = $user->id;
@@ -149,7 +164,7 @@ class FrontendAuthRegisterAction
                 $registeredUser->username = $user->username;
                 $registeredUser->save();
             }
-            
+
             DB::commit();
             $data['name'] = $user->username;
             return $contll->msgOut(true, $data);
