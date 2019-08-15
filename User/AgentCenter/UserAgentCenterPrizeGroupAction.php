7e9221857d3d34e48bfc752aabc989439c9f50b4
three <three@qq.com>
2019-08-15 05:38:12 -0400

    配置信息使用configure

--- User/AgentCenter/UserAgentCenterPrizeGroupAction.php
+++ User/AgentCenter/UserAgentCenterPrizeGroupAction.php
@@ -1,8 +1,5 @@
 <?php
-
-
 namespace App\Http\SingleActions\Frontend\User\AgentCenter;
-
 
 use App\Http\Controllers\FrontendApi\FrontendApiMainController;
 use App\Models\Admin\SystemConfiguration;
@@ -20,12 +17,12 @@ class UserAgentCenterPrizeGroupAction
     {
         $data = [];
         //最低开户奖金组
-        $data['min_user_prize_group'] = SystemConfiguration::getConfigValue('min_user_prize_group');
+        $data['min_user_prize_group'] = configure('min_user_prize_group');
         //最高开户奖金组
-        $data['max_user_prize_group'] = SystemConfiguration::getConfigValue('max_user_prize_group');
+        $data['max_user_prize_group'] = configure('max_user_prize_group');
 
-        $userInfo = $contll->currentAuth->user() ;
-        if($userInfo->prize_group < $data['max_user_prize_group'] ){
+        $userInfo = $contll->currentAuth->user();
+        if ($userInfo->prize_group < $data['max_user_prize_group']) {
             $data['max_user_prize_group'] = $userInfo->prize_group;
         }
         return $contll->msgOut(true, $data);
